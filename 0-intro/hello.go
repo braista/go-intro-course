db94f9c119b2a2dc63cf55e0a4d0df6bbfcea9fb
bs <braianstaropoli@gmail.com>
2024-01-04 19:57:37 -0300

    comments & tokens: identifiers, keywords, operators & punctuation

--- 0-intro/hello.go
+++ 0-intro/hello.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+// This is a single line comment
+// Comments are used as program documentation
+
+/*
+and this is
+a multi-line comment
+*/
+
 func main() {
 	fmt.Println("Hello World")
 }
---
+++ 0-intro/tokens.go
@@ -0,0 +1,26 @@
+package main
+
+/*
+	Tokens:
+		four classes: identifiers, keywords, operators & punctuation, and literals
+*/
+
+/*
+	Identifiers:
+		name program entities like variables and types
+		must start with a letter
+*/
+
+/*
+	Keywords:
+		reserved words that may not be used as identifiers
+		some of them:
+			break case const default else for interface select struct type
+*/
+
+/*
+	Operators & punctuation:
+		arithmetic operators:
+			+ - * / %
+		etc.
+*/
