0288d015208d090defebdf5d0a2bfc3e24c33212
bs <braianstaropoli@gmail.com>
2024-01-07 16:23:58 -0300

    go's basic types

---
+++ 3-types/types.go
@@ -0,0 +1,14 @@
+package main
+
+/**
+	Types:
+		these are the GO's basic types
+			bool
+			string
+			int		int8 	int16 	int32 (rune) 	int64
+			uint	uint8	uint16	uint32				uintptr
+			float32	float64
+			complex64	complex128
+**/
+func main() {
+}
